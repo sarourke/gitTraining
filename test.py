8a6d033fe893ee817d555a1c1051fc2704e005a1
Carter Rhea <carterrhea93@gmail.com>
2019-10-26 13:27:08 -0400

    Added test python script
    Users are asked to supply the missing code

---
+++ test.py
@@ -0,0 +1,20 @@
+'''
+Test python script for Git Training Purposes
+This program is written to ask the user their name and print it in the terminal.
+Before running this program, please fix the three minor issues :)
+After a successful run, please submit a pull request!
+'''
+
+def read_input():
+    # Please allow the user to enter their first and last name from the terminal
+    # Hint: We call this a raw input
+    first_name = ;
+    last_name = ;
+
+    # Please fix the following print statement
+    'Welcome %s %s'%(first_name,last_name)
+
+    #Supply the approriate return (We aren't actually returning anything :) )
+    return
+
+read_input()    
